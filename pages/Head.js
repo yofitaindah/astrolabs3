268af7f6d6a1d63a12b5924e9c8c71fbc50f2d10
Yofita Indah <yofitaindah@gmail.com>
2024-12-18 21:07:16 +0700

    desc-header

--- pages/Head.js
+++ pages/Head.js
@@ -5,7 +5,7 @@ const PageHead = ({ title }) => {
     <>
       <Head>
         <title>{title} || AstroLabs </title>
-        <meta name="description" content="Page Description" />
+        <meta name="description" content="Make Your Life Easier" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/logo/logo_kecil.PNG" type="image/png" />
       </Head>
